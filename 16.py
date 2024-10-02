50a14a1b5f957c1c0da0d576081cf4a05b4cc286
Joaomiguel1108 <joaomiguelmorais1108@gmail.com>
2024-10-02 20:42:10 -0300

    Create 16.py

---
+++ 16.py
@@ -0,0 +1,9 @@
+litro = int(input("digite quantas refrigerantes de 1 litro foram comprados"))
+litros2 = int(input("digite quantas refrigerantes de 2 litros foram comprados"))
+litros3 = int(input("digite quantas refrigerantes de 3 litros foram comprados"))
+
+calculo = litros2*2
+calculo2 = litros3*3
+
+total = litro + calculo2 + calculo
+print(f"você comprou {total} litros de refrigerante")
