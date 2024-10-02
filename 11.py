82201712da160b89627661de0abb948cea756f2a
Joaomiguel1108 <joaomiguelmorais1108@gmail.com>
2024-10-02 20:19:20 -0300

    Create 11.py

---
+++ 11.py
@@ -0,0 +1,11 @@
+camiseta_p = int(input("digite quantas camisetas p foram vendidas"))
+camiseta_m = int(input("digite quantas camisetas m foram vendidas"))
+camiseta_g = int(input("digite quantas camisetas g foram vendidas"))
+
+conta1 = camiseta_p *10
+conta2 = camiseta_m *12
+conta3 = camiseta_g *15
+
+resultado = camiseta_g + camiseta_m + camiseta_p
+
+print(f"o valor arrecadado foi {resultado} reais")
