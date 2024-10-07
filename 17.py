c6e5626d238adb7aac28c88a641ed2bb90082c8e
Joaomiguel1108 <joaomiguelmorais1108@gmail.com>
2024-10-07 16:49:19 -0300

    Create 17.py

---
+++ 17.py
@@ -0,0 +1,12 @@
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+
+peso1 = 2
+peso2 = 3
+
+media = (nota1 * peso1 + nota2 * peso2) / (peso1 + peso2)
+
+
+print(f"A média das notas é: {media}")
