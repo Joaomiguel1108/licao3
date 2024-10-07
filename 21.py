3c0d895f77cc365009e8902fb7bb135759a0d396
Joaomiguel1108 <joaomiguelmorais1108@gmail.com>
2024-10-07 17:17:16 -0300

    Create 21.py

---
+++ 21.py
@@ -0,0 +1,6 @@
+diagonal_maior = float(input("digite o tamanho da area maior"))
+diagonal_menor = float(input("digite o tamanho da area menor"))
+
+A = (diagonal_menor * diagonal_maior )/2
+
+print(f"A area do losango é {A}")
