acac34f1bf1784db9d11fe6aee7acbdfa0a0079c
Joaomiguel1108 <joaomiguelmorais1108@gmail.com>
2024-10-07 16:59:37 -0300

    Create 18.py

---
+++ 18.py
@@ -0,0 +1,9 @@
+salario = float(input("digite seu  salário fixo"))
+vendas =  int(input("digite quantos produtos você vendeu"))
+
+calculo = (salario*0.04)*vendas
+
+total = salario+calculo
+
+
+print(f"Seu salario total é {total}")
