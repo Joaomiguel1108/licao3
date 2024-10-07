7177428d38fa4de745f2681608d511dbe0e0f779
Joaomiguel1108 <joaomiguelmorais1108@gmail.com>
2024-10-07 16:46:29 -0300

    Create 15.py

---
+++ 15.py
@@ -0,0 +1,9 @@
+blusas = int(input("digite o número de blusas produzidas: "))
+
+
+novelos = int(input("digite o número de novelos de lã utilizados: "))
+
+
+confeccao = novelos / blusas
+
+print(f"A quantidade de novelos gastos por blusa é {confeccao}")
