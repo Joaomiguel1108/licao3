36b84ebef2ac07359e99b181411e0abe8feecff8
Joaomiguel1108 <joaomiguelmorais1108@gmail.com>
2024-10-07 17:26:56 -0300

    Create 22.py

---
+++ 22.py
@@ -0,0 +1,6 @@
+salario_minimo = 1412
+salario_funcionário = float(input("Digite o seu salario"))
+
+salarios = salario_funcionário/salario_minimo
+
+print(f"Você recebe um salario equivalente á {salarios} mínimos")
