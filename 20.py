645c9824561c358ead22d1b93541e5a42417be07
Joaomiguel1108 <joaomiguelmorais1108@gmail.com>
2024-10-07 17:15:42 -0300

    Create 20.py

---
+++ 20.py
@@ -0,0 +1,8 @@
+base_maior = float(input("digite o tamanho da area maior"))
+base_menor = float(input("digite o tamanho da area menor"))
+altura = float(input("digite a altura"))
+
+
+A = ((base_maior + base_menor) * altura)/2
+
+print(f"A area do trapézio é {A}")
